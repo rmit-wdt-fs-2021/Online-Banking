56d4ffb230a81c6d0cc842b5da43ba173ae4525f
abir1997 <s3677701@student.rmit.edu.au>
2021-01-19 03:11:52 +1100

    Added transaciton model

---
+++ InternetBanking/InternetBanking/Data/McbaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Data
+{
+    public class McbaContext : DbContext
+    {
+        public McbaContext(DbContextOptions<McbaContext> options) : base(options)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
--- InternetBanking/InternetBanking/Models/Transaction.cs
+++ InternetBanking/InternetBanking/Models/Transaction.cs
@@ -1,11 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InternetBanking.Models
 {
+    public enum TransactionType
+    {
+        Deposit = 1,
+        Withdraw = 2,
+        Transfer = 3,
+        ServiceCharge = 4
+    }
+
     public class Transaction
     {
+        public int TransactionID { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+
+        [ForeignKey("Account")]
+        public int AccountNumber { get; set; }
+        public virtual Account Account { get; set; }
+
+        [ForeignKey("DestinationAccount")]
+        public int? DestinationAccountNumber { get; set; }
+        public virtual Account DestinationAccount { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [StringLength(255)]
+        public string Comment { get; set; }
+
+        public DateTime TransactionTimeUtc { get; set; }
     }
 }
