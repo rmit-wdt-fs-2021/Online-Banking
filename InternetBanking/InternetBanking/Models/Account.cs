f6840d59915666bf54b96eddd1a51e97219349e6
abir1997 <s3677701@student.rmit.edu.au>
2021-01-19 02:31:03 +1100

    Added account model

--- InternetBanking/InternetBanking/Models/Account.cs
+++ InternetBanking/InternetBanking/Models/Account.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InternetBanking.Models
 {
+    public enum AccountType
+    {
+        Checking = 1,
+        Saving = 2
+    }
+
     public class Account
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Account Number")]
+        public int AccountNumber { get; set; }
+
+        [Display(Name = "Type")]
+        public AccountType AccountType { get; set; }
+
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [Column(TypeName = "money")]
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+        public virtual List<Transaction> Transactions { get; set; }
     }
 }
--- InternetBanking/InternetBanking/Models/Transaction.cs
+++ InternetBanking/InternetBanking/Models/Transaction.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace InternetBanking.Models
 {
-    public class Transaciton
+    public class Transaction
     {
     }
 }
