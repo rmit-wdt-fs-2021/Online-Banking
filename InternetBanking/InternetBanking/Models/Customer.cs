493e2cef87da3435167e5ce2f7e0ecaf0ee287cd
abir1997 <s3677701@student.rmit.edu.au>
2021-01-19 02:01:50 +1100

    Added customer model

---
+++ InternetBanking/InternetBanking/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(11)]
+        public string TFN { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(20)]
+        public string State { get; set; }
+
+        [StringLength(10)]
+        public string PostCode { get; set; }
+
+        [Required, StringLength(15)]
+        public string Phone { get; set; }
+    }
+}
