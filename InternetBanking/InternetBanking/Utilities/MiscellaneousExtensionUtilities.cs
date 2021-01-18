f15bcaba5619e47c36dae61ebf3a427c58b5af21
abir1997 <s3677701@student.rmit.edu.au>
2021-01-19 01:27:37 +1100

    Added utils

---
+++ InternetBanking/InternetBanking/Utilities/MiscellaneousExtensionUtilities.cs
@@ -0,0 +1,11 @@
+﻿namespace InternetBanking.Utilities
+{
+    /// <summary>
+    /// Code referenced from Tutorial 06.
+    /// </summary>
+    public static class MiscellaneousExtensionUtilities
+    {
+        public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
+        public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
+    }
+}
