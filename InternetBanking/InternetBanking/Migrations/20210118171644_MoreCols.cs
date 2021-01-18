62ef01b761d4ccfcecaea0c88c4db3814da2dac6
abir1997 <s3677701@student.rmit.edu.au>
2021-01-19 04:20:03 +1100

    Skeleton code + Db seeding done

---
+++ InternetBanking/InternetBanking/Migrations/20210118171644_MoreCols.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternetBanking.Migrations
+{
+    public partial class MoreCols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Logins",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Accounts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Logins");
+
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+    }
+}
