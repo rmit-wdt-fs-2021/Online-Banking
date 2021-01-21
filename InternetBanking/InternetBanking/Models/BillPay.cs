d1ae338d51cd4317278ea3237ac67bf75f592dae
abir1997 <s3677701@student.rmit.edu.au>
2021-01-22 04:03:26 +1100

    Bill pay VM added

--- InternetBanking/InternetBanking/Models/BillPay.cs
+++ InternetBanking/InternetBanking/Models/BillPay.cs
@@ -21,8 +21,8 @@ namespace InternetBanking.Models
         [Column(TypeName = "decimal(18, 2)"), DataType(DataType.Currency)]
         public decimal Amount { get; init; }
 
-        [DataType(DataType.Date)]
-        [Display(Name = "Scheduled Date")]
+        [Required]
+        [DataType(DataType.Date), Display(Name = "Scheduled Date")]
         public DateTime ScheduledDate { get; init; }
 
         public BillPeriod BillPeriod { get; init; } = BillPeriod.OneTime;
---
+++ InternetBanking/InternetBanking/ViewModels/BillPayViewModel.cs
@@ -0,0 +1,24 @@
+﻿using InternetBanking.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InternetBanking.ViewModels
+{
+    public class BillPayViewModel
+    {
+        [Display(Name = "From Account")]
+        public Account FromAccount { get; set; }
+
+        [Required, Display(Name = "To Payee")]
+        public string ToPayee { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required , Display(Name = "Scheduled Date")]
+        public DateTime ScheduledDate { get; set; }
+
+        [Required]
+        public BillPeriod BillPeriod { get; set; }
+    }
+}
