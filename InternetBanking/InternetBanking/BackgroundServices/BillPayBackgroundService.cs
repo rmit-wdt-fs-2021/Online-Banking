4c23ef82702d428ebc970d362c6f5e1be8d66e5d
abir1997 <s3677701@student.rmit.edu.au>
2021-01-22 01:43:30 +1100

    Bill pay WIP

---
+++ InternetBanking/InternetBanking/BackgroundServices/BillPayBackgroundService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InternetBanking.BackgroundServices
+{
+    public class BillPayBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _services;
+        private readonly ILogger<BillPayBackgroundService> _logger;
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
