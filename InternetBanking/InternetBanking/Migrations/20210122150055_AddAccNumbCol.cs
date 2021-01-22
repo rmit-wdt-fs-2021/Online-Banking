2c2b6f997f5d95d89eb8ec25e4c8d7bfd082a766
abir1997 <s3677701@student.rmit.edu.au>
2021-01-23 02:03:30 +1100

    Added missing col to BillPay

---
+++ InternetBanking/InternetBanking/Migrations/20210122150055_AddAccNumbCol.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternetBanking.Migrations
+{
+    public partial class AddAccNumbCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccountNumber",
+                table: "BillPay",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillPay_AccountNumber",
+                table: "BillPay",
+                column: "AccountNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BillPay_Accounts_AccountNumber",
+                table: "BillPay",
+                column: "AccountNumber",
+                principalTable: "Accounts",
+                principalColumn: "AccountNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BillPay_Accounts_AccountNumber",
+                table: "BillPay");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BillPay_AccountNumber",
+                table: "BillPay");
+
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "BillPay");
+        }
+    }
+}
