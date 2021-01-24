17d0620016cf4ea013582c31e2627c32aa9232d1
abir1997 <s3677701@student.rmit.edu.au>
2021-01-25 03:08:05 +1100

    Added AdminMvc app

---
+++ InternetBanking/AdminApi/AdminApi/Models/Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApi.Models.Repository
+{
+    public interface IDataRepository
+    {
+        // What do u want to do to database?
+        // 1. View Transaction history.
+        // 2. 
+    }
+}
