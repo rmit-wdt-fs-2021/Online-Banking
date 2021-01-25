9c3fa311dce4b1422d17a1588b10df6d53d0fb0d
abir1997 <s3677701@student.rmit.edu.au>
2021-01-25 21:50:38 +1100

    Added context in api

--- InternetBanking/AdminApi/AdminApi/Data/McbaContext.cs
+++ InternetBanking/AdminApi/AdminApi/Data/McbaContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AdminApi.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,11 @@ namespace AdminApi.Data
 {
     public class McbaContext : DbContext
     {
+        public McbaContext(DbContextOptions<McbaContext> options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<BillPay> BillPay { get; set; }
+        public DbSet<Login> Logins { get; set; }
     }
 }
