26cbe2df78ec3a8ddfc690a8e970856a5681105d
abir1997 <s3677701@student.rmit.edu.au>
2021-01-26 02:14:30 +1100

    Added admin filter

---
+++ InternetBanking/AdminApi/AdminApp/Controllers/TransactionController.cs
@@ -0,0 +1,13 @@
+﻿using AdminApp.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApp.Controllers
+{
+    [AuthorizeAdmin]
+    public class TransactionController
+    {
+    }
+}
---
+++ InternetBanking/AdminApi/AdminApp/Filters/AuthorizeAdminAttribute.cs
@@ -0,0 +1,18 @@
+﻿using AdminApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace AdminApp.Filters
+{
+    public class AuthorizeAdminAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.Session.GetString(nameof(Admin.Username));
+            if (!string.IsNullOrEmpty(user))
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
