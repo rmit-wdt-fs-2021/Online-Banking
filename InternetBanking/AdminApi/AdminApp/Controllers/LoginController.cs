4ad8ca12bdb50cbec9de9072ee34ecec74f91dc8
abir1997 <s3677701@student.rmit.edu.au>
2021-01-26 02:02:44 +1100

    Admin login done

---
+++ InternetBanking/AdminApi/AdminApp/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using AdminApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AdminApp.Controllers
+{
+    [Route("/Mcba/SecureAdminLogin")]
+    public class LoginController : Controller
+    {
+
+        private const string AdminUsername = "admin";
+        private const string AdminPassword= "admin";
+
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == AdminPassword && password == AdminPassword)
+            {
+                HttpContext.Session.SetString(nameof(Admin.Username), AdminUsername);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("LoginFailed", "Login failed, please try again.");
+                return View(new Login { Username = username });
+            }
+        }
+
+        [Route("LogoutNow")]
+        public IActionResult Logout()
+        {
+            // Logout admin.
+            HttpContext.Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
