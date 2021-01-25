3ae3fb29aaa9cb0fe99dff008345d84d2a8ce31a
abir1997 <s3677701@student.rmit.edu.au>
2021-01-26 02:32:22 +1100

    Transaciton controller WIP

--- InternetBanking/AdminApi/AdminApp/Controllers/TransactionController.cs
+++ InternetBanking/AdminApi/AdminApp/Controllers/TransactionController.cs
@@ -1,13 +1,27 @@
 ﻿using AdminApp.Filters;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AdminApp.Controllers
 {
     [AuthorizeAdmin]
-    public class TransactionController
+    public class TransactionController : Controller
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private HttpClient Client => _clientFactory.CreateClient("api");
+
+        public TransactionController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return null;
+        }
     }
 }
