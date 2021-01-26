8c16cddeb70ba8a38b7de8fdfdcb143d755b0929
abir1997 <s3677701@student.rmit.edu.au>
2021-01-26 14:23:55 +1100

    Boolean fields added

---
+++ InternetBanking/InternetBanking/Migrations/20210126023129_AddedFlags.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternetBanking.Migrations
+{
+    public partial class AddedFlags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "Logins",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBlocked",
+                table: "BillPay",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "Logins");
+
+            migrationBuilder.DropColumn(
+                name: "IsBlocked",
+                table: "BillPay");
+        }
+    }
+}
