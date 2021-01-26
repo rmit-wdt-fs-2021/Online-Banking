352a76aaf54f451dc207125b68402d95c10dfb34
Aidan Holland <aidanjames11@hotmail.com>
2021-01-26 19:56:44 +1100

    Added custom error page

---
+++ InternetBanking/InternetBanking/Controllers/ErrorCodeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetBanking.Controllers
+{
+    public class ErrorCodeController : Controller
+    {
+        //Taken from lecture 7
+
+        [HttpGet("ErrorCode/{errorCode}")]
+        public IActionResult Index(int errorCode)
+        {
+            return View(errorCode);
+        }
+    }
+}
--- InternetBanking/InternetBanking/Startup.cs
+++ InternetBanking/InternetBanking/Startup.cs
@@ -62,6 +62,8 @@ namespace InternetBanking
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/ErrorCode/{0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
